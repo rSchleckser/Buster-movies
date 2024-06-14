c959f8fd604fd2946c642f70477ee6a02f9f376d
rSchleckser <rickyschleckser@gmail.com>
2024-06-14 01:15:14 -0700

    feat: add media queries to tv and movie show page, movie and tv main page, and adjust edit /delete btn in review page

--- route/search.js
+++ route/search.js
@@ -13,4 +13,14 @@ router.post('/:userId/results', isLoggedIn, postToSearchList);
 //@desc PUT - Update movies to watchlist/favorites
 router.put('/:userId/results', isLoggedIn, updateSearchList);
 
+// Movies Search
+// // @desc  GET user search results page
+// router.get('/:userId/results', isLoggedIn, getSearchResults);
+
+// //@desc POST - add movies to watchlist/favorites
+// router.post('/:userId/results', isLoggedIn, postToSearchList);
+
+// //@desc PUT - Update movies to watchlist/favorites
+// router.put('/:userId/results', isLoggedIn, updateSearchList);
+
 module.exports = router;
